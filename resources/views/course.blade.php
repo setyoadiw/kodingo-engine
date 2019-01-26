da0d7312bae698b61000c6b67aa67815b1ae6535
ZarelLast <lastzarel@gmail.com>
2019-01-26 17:41:26 +0700

    video

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,65 +1,117 @@
 @extends('layouts.nav')
 @section('course')
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 <style>
-.side{
-    height:100%; 
+.link-vid{
+    position:relative;
+    width:100%;
+    height:60px;
+    background:#fff;
+    border-bottom:solid 1px #e3e3e3;
+    color:#6677eb;
 }
-#leftCol {
-    position: absolute;
-    width: 95%;
+.link-vid:hover{
+    color:#6677eb;
+}
+.link-vid span{
+    position:relative;
+    top:25%;
+    bottom:25%;
+    left:10%;
+}
+.title-vid{
+    width:100%;
+    height:60px;
+    background:#fff;
+    border-bottom:solid 1px #e3e3e3;
+    color:#919095;
+}
+.title-vid span{
+    position:relative;
+    top:30%;
+    bottom:30%;
+    left:35%;
+}
+.scroll-div{
+    width:100%;
     height:100%;
-    background-color: #DDD; 
-    font-size: 21px;
-    overflow-y: scroll;
-    top: 0;
-    bottom: 0;
-}
-.nav-link{
-    border-bottom:solid 1px #c3c4c4;
-    color:black;
-    font-size:14px;
-}
-.nav-link:hover{
-    color:black;
+    padding:0;
+    overflow-y:scroll;
 }
 </style>
-<div class="row" style="height:100%">
-    <div class="col-sm-2 row-sm side" style="padding-right:0;">
-        <nav id="leftCol">
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        </nav>
-    </div>
 
-    <div class="col-sm" style="padding:0">
-        <nav class="navbar navbar-light bg-light">
-            <span class="navbar-brand">Video 1</span>
-            <span>LARAVEL PERFROMANCE</span>
-            <span>Episode 1</span>
-        </nav>
-        
-        <div class="container">
-            <div style="padding:10px 0;margin:20px 0;border:solid 1px black" class="d-flex justify-content-end">
-                <a>video 2</a>
+<div style="width:100%;height:auto">
+    <div class="row" style="width:100%;;margin:0;">
+        <div class="col-md-3 scroll-div">
+            <div style="width:100%;height:100%">
+                <div class="title-vid">
+                    <span><b>SUB JUDUL 1</b></span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 1</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 2</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 3</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 4</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 5</span>
+                </div>
+                <div class="title-vid">
+                    <span><b>SUB JUDUL 2</b></span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 1</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 2</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 3</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 4</span>
+                </div>
+                <div class="link-vid" href="">
+                    <span>Judul 5</span>
+                </div>
             </div>
-            <iframe width="420" height="315"
-                src="https://player.vimeo.com/external/219608985.hd.mp4?s=084e17ed7cd8fc23d69a093f1ef7b95817dc6fe9&amp;profile_id=119"><source src="https://player.vimeo.com/external/219608985.hd.mp4?s=084e17ed7cd8fc23d69a093f1ef7b95817dc6fe9&amp;profile_id=119">
-            </iframe>
+        </div>
+        <div class="col-md-9" style="width:100px;height:100%;padding:0;position:relative">
+            <nav class="navbar navbar-light bg-light" style="width:100%;border-bottom:solid 1px #e3e3e3">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <h2>Judul 1</h2>
+                    </a>
+                    <span><h6>Episode 1</h6></span>
+                </div>
+                <span>Sub Judul</span>
+            </nav>
+            <div class="container" style="padding-top:5%;width:90%;position:relative;height:100%">
+                <div style="border:solid 1px #e5e5e5;padding:10px;">
+                    <div class="text-right">
+                        <span>Judul 2</span>
+                    </div>
+                </div>
+
+                <div style="width:100%;height:100%;position:relative;padding-top:5%">
+                    <iframe width="100%" height="480px" src="https://www.youtube.com/embed/Oryq_TdAyNI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe>
+                    <div style="padding-top:5%"></div>
+                </div>
+                        
+            </div>
         </div>
     </div>
 </div>
-
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,7 +3,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-light" style="background:#373e46;width:100%;">
+<nav class="navbar navbar-light" style="background:#373e46;width:100%;position:relative">
     <a class="navbar-brand" style="color:white;" href="#">Scaling Laravel</a>
     <a style="color:white;">My Course</a>
     <div class="btn-group">
